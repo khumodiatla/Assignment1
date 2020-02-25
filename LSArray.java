2784dc7580c7d8a742658fcb65e341be6af21834
khumodiatla <mtlkhu004@sl-dual-253.cs.uct.ac.za>
2020-02-25 18:14:54 +0200

    I edited LSArray.java

---
+++ LSArray.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.util.Scanner;
+class LSArrayApp
+{
+    private static String[] array;
+    private static int operationCount=0;
+
+    public static void main(String[] args)
+    {
+        array = new String[2976];
+        
+        try
+        {
+            File file = new File("C:\\Users\\Masego\\IdeaProjects\\prj1\\src\\Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt");
+            Scanner scannerReader = new Scanner(file);
+
+
+
+            while(scannerReader.hasNextLine())
+            {
+                for(int i=0; i<89;i++)
+                {
+                    array[i] = scannerReader.nextLine();
+                }
+            }
+            scannerReader.close();
+        }
+
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        LSArrayApp p = new LSArrayApp();
+        //printAreas(args[0],args[1],args[2]);
+        p.printAreas(args[0],args[1],args[2]);
+        //p.printAllAreas();
+       //System.out.println( printAreas(args[0],args[1],args[2]));
+
+    }
+
+    public void printAreas(String stage, String day, String startTime)
+    {
+
+                String key = stage + "_" + day + "_" + startTime;
+                int x = key.length();
+                int noAreaCount=0;
+                int operationCount =0;
+                
+                for(int i =0;i<array.length;i++)
+                {
+                    if(key==array[i].substring(0,x))
+                    {
+                    	noAreaCount++;
+                    	operationCount++;
+                    	System.out.println(array[i].substring(x+1));
+                    }
+                
+                    else if(!(noAreaCount==0))
+                        {
+                        System.out.println("Areas not found");
+                        //break;
+                        }
+                }
+            
+        //System.out.println(operationCount);
+
+    }
+
+
+    public void printAllAreas()
+    {
+        for (int i =0;i<array.length;i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
